e432fa5427e200ced567224e1d24291c36e5fde4
Di-Kar <mitry.boss@gmail.com>
2026-06-24 13:31:00 +0700

    Add files via upload

---
+++ tests/functional/utils/wait_for_es.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import sys
+import elasticsearch
+
+from backoff import backoff
+# Добавление пути к файлу настроек, т.к. запускается как скрипт, а не модуль
+sys.path.insert(0, str(Path(__file__).resolve().parents[3]))
+from tests.functional.settings import test_settings
+
+@backoff(elasticsearch.ConnectionError)
+def ping_elastic(client):
+    if not client.ping():
+        raise elasticsearch.ConnectionError("Elasticsearch don't response")
+    return "Elastic is available."
+
+
+if __name__ == '__main__':
+    elastic_client = elasticsearch.Elasticsearch(
+        hosts=test_settings.elastic_settings.get_host(),
+        verify_certs=False,
+        ssl_show_warn=False 
+    )
+    try:
+        result = ping_elastic(elastic_client)
+        print(result)
+    except (elasticsearch.ConnectionError, TimeoutError) as e:
+        print(e)
---
+++ tests/functional/utils/wait_for_redis.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+
+import redis
+from backoff import backoff
+
+# Добавление пути к файлу настроек, т.к. запускается как скрипт, а не модуль
+sys.path.insert(0, str(Path(__file__).resolve().parents[3]))
+from tests.functional.settings import test_settings
+
+@backoff(redis.ConnectionError)
+def ping_redis(client):
+    if not client.ping():
+        raise redis.ConnectionError("Redis don't response")
+    return "Redis is available."
+
+
+if __name__ == '__main__':
+    redis_client = redis.Redis(
+        host=test_settings.redis_settings.host,
+        port=test_settings.redis_settings.port
+    )
+    try:
+        result = ping_redis(redis_client)
+        print(result)
+    except (redis.ConnectionError, TimeoutError) as e:
+        print(e)
